70d73ff46e66a3acf060c2233f9e46a768485f44
Pierre Vandekerckhove <pierre4vdk@gmail.com>
2022-11-02 15:42:03 +0100

    After running lint fix

--- src/features/createCanvas.js
+++ src/features/createCanvas.js
@@ -1,84 +1,69 @@
-import * as THREE from "three";
-import {gsap} from "gsap";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
-
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import * as THREE from 'three'
+gsap.registerPlugin(ScrollTrigger)
 
 function createCanvas() {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer()
   const badge = document.createElement('div')
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  badge.appendChild( renderer.domElement );
-  badge.classList.add('canvas-container');
-  document.body.appendChild( badge );
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  badge.appendChild(renderer.domElement)
+  badge.classList.add('canvas-container')
+  document.body.appendChild(badge)
 
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
 
+  const ambientLight = new THREE.AmbientLight(0x000000)
+  scene.add(ambientLight)
 
-  const ambientLight = new THREE.AmbientLight( 0x000000 );
-  scene.add( ambientLight );
+  const light1 = new THREE.PointLight(0xffffff, 1, 0)
+  light1.position.set(0, 200, 0)
+  scene.add(light1)
 
-  const light1 = new THREE.PointLight( 0xffffff, 1, 0 );
-  light1.position.set( 0, 200, 0 );
-  scene.add( light1 );
+  const light2 = new THREE.PointLight(0xffffff, 1, 0)
+  light2.position.set(100, 200, 100)
+  scene.add(light2)
 
-  const light2 = new THREE.PointLight( 0xffffff, 1, 0 );
-  light2.position.set( 100, 200, 100 );
-  scene.add( light2 );
+  const light3 = new THREE.PointLight(0xffffff, 1, 0)
+  light3.position.set(-100, -200, -100)
+  scene.add(light3)
 
-  const light3 = new THREE.PointLight( 0xffffff, 1, 0 );
-  light3.position.set( - 100, - 200, - 100 );
-  scene.add( light3 );
-
-  camera.position.z = 5;
+  camera.position.z = 5
 
   function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-
-
-
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
   }
 
-
-  animate();
-
+  animate()
 
   //trigger test
 
   gsap.to('.scroll__progress span', {
-    width: "100%",
+    width: '100%',
     ease: 'none',
     scrollTrigger: { scrub: 0.3 },
-    onUpdate: function () {
-
-
-
-
-    }
-  });
+    onUpdate: function () {},
+  })
 
   gsap.to(cube.rotation, {
     x: 3.14,
     y: 3.14,
     ease: 'none',
     scrollTrigger: { scrub: 0.3 },
-    onUpdate: function () {
-
-
-
-
-    }
-  });
-
-
+    onUpdate: function () {},
+  })
 }
 
 export default createCanvas

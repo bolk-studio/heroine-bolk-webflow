3e86911caba5d20cec56b154e66790907e76c9bc
Daphné HB <DaphneHB@users.noreply.github.com>
2023-04-27 14:42:27 +0200

    Correct hero sound script

--- src/gsap_scripts/script_hero_sound.js
+++ src/gsap_scripts/script_hero_sound.js
@@ -1,125 +1,92 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin();
+var SOUND_PLAYING = false;
+var audio_elt, animation_white;
 
-  // Conviction Page
-  // Hero Conviction Movement on Page Load
-  let convictionHero = gsap.timeline({ ease: "power1.inOut" });
-  convictionHero
-    .to("[opacity-null]", {
-      opacity: 1,
-      duration: 0
-    })
-    .from(".conviction-hero_heading-wrapper.is_one", {
-      x: "-50vw",
-      duration: 1.3
-    })
-    .from(
-      ".conviction-hero_heading-wrapper.is_two,.conviction-hero_subtitle-wrapper",
-      {
-        x: "50vw",
-        duration: 1.3,
-        stagger: 0.2
-      },
-      "<"
-    )
-    .from(
-      ".conviction-hero_image-wrapper",
-      {
-        y: "50vh",
-        scale: 0.5,
-        duration: 1
-      },
-      "<"
-    )
-    .to(
-      ".conviction-hero_image",
-      {
-        scale: 1.1,
-        duration: 1
-      },
-      "<"
-    );
+function play_audio() {
+  audio_elt.play();
+  animation_white.play();
+  SOUND_PLAYING = true;
+}
+function stop_audio() {
+  audio_elt.pause();
+  animation_white.stop();
+  SOUND_PLAYING = false;
+}
 
-  // Conviction Page
-  // Hero Conviction Movement on Scroll OUT
+function activate_audio() {
+  play_audio();
+}
+function deactivate_audio() {
+  stop_audio();
+}
 
-  if (window.innerWidth > 992) {
-    let convictionHeroOut = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".section-conviction_hero  ",
-        start: "bottom 95%",
-        end: "bottom top",
-        scrub: 1
-      }
-    });
-    convictionHeroOut
-      .to(".conviction-hero_heading-wrapper.is_one", {
-        x: "50vw",
-        duration: 1.3
-      })
-      .to(
-        ".conviction-hero_heading-wrapper.is_two,.conviction-hero_subtitle-wrapper",
-        {
-          x: "-50vw",
-          duration: 1.3
-        },
-        "<"
-      )
-      .to(
-        ".conviction-hero_image",
-        {
-          scale: 1,
-          duration: 1
-        },
-        "<"
-      );
-  }
+// Waiting until every element has loaded
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  audio_elt = document.getElementById("page-audio");
+  audio_elt.setAttribute("src", SOUND_FILE);
+  //const audio_play_button = document.querySelectorAll('[button-sound]');
+  audio_elt.load();
+  audio_elt.volume = 0.05;
 
-  // Conviction Parallax Element On Scroll
-  $("[content-conviction]").each(function (index) {
-    let triggerContent = $(this),
-      overline = $(this).find("[conviction='overline']"),
-      heading = $(this).find("[conviction='heading']"),
-      subtitle = $(this).find("[conviction='subtitle']"),
-      text = $(this).find("[conviction='texte']"),
-      card = $(this).find("[conviction='card']");
+  var lottieWrapper_parent = document.getElementById("lottie-sound_inner");
+  var whiteLottieWrapper = document.getElementById(
+    "lottie-sound-white_wrapper"
+  );
 
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: triggerContent,
-        start: "top bottom",
-        end: "top top",
-        scrub: true
-      }
-    });
-    tl.from(overline, { y: "150rem", duration: 1 }, "<")
-      .from(heading, { y: "250rem", duration: 1 }, "<")
-      .from(subtitle, { y: "350rem", duration: 1 }, "<")
-      .from(text, { y: "400rem", stagger: { each: "0.08" }, duration: 1 }, "<")
-      .from(
-        card,
-        { y: "450rem", stagger: { each: "0.08" }, duration: 1.3 },
-        "<"
-      );
-    if (window.innerWidth > 992) {
-      let tl2 = gsap.timeline({
-        scrollTrigger: {
-          trigger: triggerContent,
-          start: "top -20%",
-          end: "bottom top",
-          scrub: true
-        }
-      });
-      tl2
-        .to(overline, { y: "-400rem", duration: 1 }, "<")
-        .to(heading, { y: "-300rem", duration: 1 }, "<")
-        .to(subtitle, { y: "-200rem", duration: 1 }, "<")
-        .to(text, { y: "-100rem", stagger: { each: "0.08" }, duration: 1 }, "<")
-        .to(
-          card,
-          { y: "-200rem", stagger: { each: "0.08" }, duration: 1.3 },
-          "<"
-        );
+  animation_white = lottie.loadAnimation({
+    container: whiteLottieWrapper,
+    path:
+      "https://uploads-ssl.webflow.com/635804dcb2f8dac9c2128174/6407a3f549610e9030b17094_tugBTXmmQr.json",
+    renderer: "canvas",
+    loop: true,
+    autoplay: SOUND_PLAYING,
+    name: "Lottie White Sound Anim"
+  });
+
+  lottieWrapper_parent.addEventListener("click", function (event) {
+    if (SOUND_PLAYING) {
+      stop_audio();
+    } else {
+      play_audio();
     }
   });
+  // Event Listeners
+  /*var observerSoundOff = new IntersectionObserver(
+    function (entries) {
+      if (entries[0].isIntersecting === true) {
+        activate_audio();
+      } else {
+        deactivate_audio();
+      }
+    },
+    { threshold: [0.5] }
+  );
+  document
+    .querySelectorAll(".section-home_hero")
+    .forEach((element) => observerSoundOff.observe(element));*/
+
+  // HomePage Sound according to Hero Section visibility
+  /*let soundActivationHero = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".section-home_hero",
+      start: "bottom 97%",
+      end: "bottom 80%",
+      scrub: 1
+    }
+  });*/
+  ScrollTrigger.create({
+    trigger: ".section-home_hero",
+    start: "bottom 97%",
+    end: "bottom 80%",
+    onLeave: () => {
+      stop_audio();
+    }
+  });
+  /*function first_launch_site_sound(){
+      lottieWrapper_parent.click();
+      audio_elt.play();
+      document.removeEventListener("mousemove",first_launch_site_sound);
+    }
+    // Playing the sound at load of the page
+    document.addEventListener("mousemove", first_launch_site_sound);*/
 });
--- src/hero/hero_audio.js
+++
@@ -1,6 +0,0 @@
-
-function first_launch_site_sound() {
-    if (window.sessionStorage.getItem('firstload') == null) {
-        play_audio();
-    }
-}

c9a17628e058a56fec073499127657ca06307795
Daphné HB <DaphneHB@users.noreply.github.com>
2023-05-15 10:03:34 +0200

    Test changing ARticfact url back to relative url

--- heroine/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// It is handy to not have those transformations while we developing
-module.exports = {
-    plugins: [
-        require('autoprefixer'),
-        require('cssnano'),
-        // More postCSS modules here if needed
-    ]
-}
